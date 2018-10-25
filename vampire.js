095914f8ae978aba21becd99588499e715c2f605
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-10-25 19:24:53 +0000

    vamps

--- vampire.js
+++ vampire.js
@@ -10,22 +10,32 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    vampire.creator = this;
+    this.offspring.push(vampire);
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 1;
+    if(this.creator === null) {
+      return 0;
+    } else {
+      return count + this.creator.numberOfVampiresFromOriginal;
+    }
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -36,6 +46,49 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+
+    if(vampire === this){
+      return this;
+    }
+
+    let myAncestors = [this];
+    let vampAncestors = [vampire];
+    let currentVamp;
+
+    currentVamp = this;
+
+    while(currentVamp.creator != null) {
+      currentVamp = currentVamp.creator;
+      myAncestors.push(currentVamp);
+
+    }
+
+    currentVamp = vampire;
+
+    while(currentVamp.creator != null) {
+      currentVamp = currentVamp.creator;
+      vampAncestors.push(currentVamp);
+    }
+
+    // console.log("myAncestors:",myAncestors);
+    // console.log("vampAncestors:",vampAncestors);
+
+    let myAnc;
+    let vampAnc;
+    let lastCommon = myAncestors[myAncestors.length - 1]; //set last common to root
+
+    // console.log("lastCommon-init",lastCommon.name);
+
+    while(myAncestors.length >= 0) {
+      myAnc = myAncestors.pop();
+      vampAnc = vampAncestors.pop();
+
+      if(myAnc != vampAnc) {
+        return lastCommon;
+      } else {
+        lastCommon = myAnc;
+      }
+    }
 
   }
 }
